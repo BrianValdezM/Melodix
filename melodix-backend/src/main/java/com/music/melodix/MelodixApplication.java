bbbc4e385b1a3633c97b051d81c6fcb3d8e23baa
Brian Omar Valdez Mendoza <brayy_vxm@hotmail.com>
2026-04-07 09:31:32 -0600

    chore: initial project setup with Spring Boot

---
+++ melodix-backend/src/main/java/com/music/melodix/MelodixApplication.java
@@ -0,0 +1,13 @@
+package com.music.melodix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelodixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelodixApplication.class, args);
+	}
+
+}
---
+++ melodix-backend/src/test/java/com/music/melodix/MelodixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.melodix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
