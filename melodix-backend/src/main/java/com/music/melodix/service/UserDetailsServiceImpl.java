7eb3b52a5f24da226353132cbc44b5d84ff8e64f
Brian Omar Valdez Mendoza <brayy_vxm@hotmail.com>
2026-04-07 10:23:58 -0600

    feat: add UserRepository, AuthService and UserDetailsService

---
+++ melodix-backend/src/main/java/com/music/melodix/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.music.melodix.service;
+
+import com.music.melodix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+            user.getEmail(),
+            user.getPassword(),
+            Collections.emptyList()
+        );
+    }
+}
