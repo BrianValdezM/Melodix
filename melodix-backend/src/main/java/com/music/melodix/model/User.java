3a80d3522074845a0f92ad9843abcd5d903f0b94
Brian Omar Valdez Mendoza <brayy_vxm@hotmail.com>
2026-04-07 09:47:40 -0600

    feat: add User entity with playlists and favorites relations

---
+++ melodix-backend/src/main/java/com/music/melodix/model/User.java
@@ -0,0 +1,39 @@
+package com.music.melodix.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String username;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String avatarUrl;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Playlist> playlists;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Favorite> favorites;
+}
