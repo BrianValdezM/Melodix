8ef401212c3e4e7f8efec43774723f2871f1b765
Brian Omar Valdez Mendoza <brayy_vxm@hotmail.com>
2026-04-08 11:42:26 -0600

    feat: add PlaylistRepository and FavoriteRepository

---
+++ melodix-backend/src/main/java/com/music/melodix/repository/PlaylistRepository.java
@@ -0,0 +1,9 @@
+package com.music.melodix.repository;
+
+import com.music.melodix.model.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+    List<Playlist> findByUserId(Long userId);
+}
